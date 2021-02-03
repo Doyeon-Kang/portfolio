4306743ad10236b1e7583f7c640b671485485ce9
Doyeon-Kang <56114680+Doyeon-Kang@users.noreply.github.com>
2021-02-03 18:06:28 +0900

    JS: Home contact button

--- main.js
+++ main.js
@@ -15,11 +15,20 @@ document.addEventListener('scroll', () => {
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', (event) => {
     const target = event.target;
-    const link = event.target.dataset.link;
+    const link = target.dataset.link;
     if(link == null) {
         return;
     }
-    console.log(event.target.dataset.link);
-    const scrollTo = document.querySelector(link);
-    scrollTo.scrollIntoView({behavior: 'smooth'});
+    scrollIntoView(link);
 });
+
+// Handle click on "contact me" button on home
+const contact = document.querySelector('.home__contact');
+contact.addEventListener('click', () => {
+    scrollIntoView('#contact')
+});
+
+function scrollIntoView(selector) {
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({behavior: 'smooth'});
+}
