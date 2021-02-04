aeaf47764cea635b3ff8f22ed9ff55b49f498918
Doyeon-Kang <56114680+Doyeon-Kang@users.noreply.github.com>
2021-02-04 10:26:59 +0900

    JS: Arrow up

--- main.js
+++ main.js
@@ -35,7 +35,18 @@ document.addEventListener('scroll', () => {
     home.style.opacity = 1 - (window.scrollY / homeHeight);
 });
 
-
+// Show "arrow up" button when scrolling down
+const arrowUp = document.querySelector('.arrow-up');
+document.addEventListener('scroll', () => {
+    if(window.scrollY > (homeHeight/2)) {
+        arrowUp.classList.add('visible');
+    } else {
+        arrowUp.classList.remove('visible');
+    }
+})
+arrowUp.addEventListener('click', () => {
+    scrollIntoView('#home');
+})
 
 
 
